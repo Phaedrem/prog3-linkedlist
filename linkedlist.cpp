0e3c8fa99c51041b700236092252029be0e4ebd0
Darren Bowers <darren.bowers@g.austincc.edu>
2022-03-07 12:04:10 -0600

    initial commit

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,44 @@
-/*
- * your comment header here
- */
+/*********************
+Name: Darren Bowers
+Coding 05
+Purpose: Introduction to Linked Lists
+**********************/
+
+#include "linkedlist.h"
+
+
+LinkedList::LinkedList(){
+    head = NULL;
+}
+
+LinkedList::~LinkedList(){
+    delete head; 
+}
+
+bool LinkedList::addNode(int id, string* info){
+    return true;
+}
+
+bool LinkedList::deleteNode(int id){
+    return true;
+}
+
+bool LinkedList::getNode(int id, Data* returnBox){
+    return true;
+}
+
+void LinkedList::printList(bool direction){
+
+}
+
+int LinkedList::getCount(){
+    return 0;
+}
+
+bool LinkedList::clearList(){
+    return true;
+}
+
+bool LinkedList::exists(int id){
+    return true;
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,41 @@
-/*
- * your comment header here
- */
+/*********************
+Name: Darren Bowers
+Coding 05
+Purpose: Introduction to Linked Lists
+**********************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <iostream>
+#include "data.h" 
+
+using std::cout;
+using std::endl;
+
+class LinkedList {
+
+public:
+    LinkedList();
+    ~LinkedList();
+    bool addNode(int, string*);
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    void printList(bool = false);
+    int getCount();
+    bool clearList();
+    bool exists(int);
+
+
+private:
+    Node *head;
+
+
+
+
+
+
+
+};
+
+#endif //LINKED_LIST_H

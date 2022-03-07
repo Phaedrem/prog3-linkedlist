c4bad6dc628d3af576f2ce8287b49d08da8270ac
Darren Bowers <darren.bowers@g.austincc.edu>
2022-03-07 14:26:12 -0600

    wokred out printList to loop through list and print if head is not = to NULL. Put in a temporary destructor for testing

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -8,11 +8,17 @@ Purpose: Introduction to Linked Lists
 
 
 LinkedList::LinkedList(){
-    head = NULL;
+    head = NULL; 
 }
 
 LinkedList::~LinkedList(){
-    delete head; 
+    Node *current = head;
+    while(current != NULL){
+        Node *next = current->next;
+        delete current;
+        current = next; 
+    }
+    head = NULL; 
 }
 
 bool LinkedList::addNode(int id, string* info){
@@ -28,6 +34,17 @@ bool LinkedList::getNode(int id, Data* returnBox){
 }
 
 void LinkedList::printList(bool direction){
+    if(head != NULL){
+        int i = 1;
+        Node *current = head;
+        while(current != NULL){
+            cout << i << ": " <<current->data.id << " : " << current->data.data << endl;
+            i++;
+            current = current->next;
+        }
+    }else{
+        cout << "List is empty" << endl;
+    }
 
 }
 
